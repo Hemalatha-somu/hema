833bdc4d154fe84e2997bdbebf4091ac190fb2b8
Hemalatha-somu <heme96.hs@gmail.com>
2016-07-06 11:46:02 +0530

    Create vowel_consonant.c

---
+++ vowel_consonant.c
@@ -0,0 +1,10 @@
+void main()
+{
+char ch;
+printf("Enter the character");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+printf("\nvowel");
+else
+printf("\n consonant");
+}
